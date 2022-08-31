e69a2f2467c91bfc9d79b707bfdbe1aa332e71fd
Hilary <hilarykodji@gmail.com>
2022-08-31 13:47:07 +0000

    [dev] - change relationship between user and email verification token models

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -42,8 +42,8 @@ export default class User extends BaseModel {
     }
   }
 
-  @hasMany(() => EmailVerificationToken)
-  public emailVerificationTokens: HasMany<typeof EmailVerificationToken>
+  @hasOne(() => EmailVerificationToken)
+  public emailVerificationTokens: HasOne<typeof EmailVerificationToken>
 
   @hasMany(() => PasswordResetToken)
   public passwordResetTokens: HasMany<typeof PasswordResetToken>
