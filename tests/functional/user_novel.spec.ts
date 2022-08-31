d35cc408f7001fe1decb3aff633564a5e27499e7
Hilary <hilarykodji@gmail.com>
2022-08-31 13:11:47 +0000

    [test] - add missing tests for user update of novels

--- tests/functional/user_novel.spec.ts
+++ tests/functional/user_novel.spec.ts
@@ -122,4 +122,67 @@ test.group('User novels', (group) => {
     response.assertAgainstApiSpec()
   })
 
+  test("unverified user cannot update novel", async ({ client }) => {
+    const user = await UserFactory
+      .with("emailVerificationTokens", 1)
+      .with("novels", 1)
+      .create()
+
+    const novel = await user.related("novels").query().firstOrFail()
+
+    const payload = {
+      title: "someRandomTitle",
+      content: [
+        {
+          title: "A new day",
+          content: "Once there was an innocent man..."
+        },
+        {
+          title: "At dawn's end",
+          content: "... but alas, everything comes to an end."
+        }
+      ],
+      tags: ["self-discovery", "adventure"]
+    }
+
+    const response = await client
+      .put(`/api/v1/user/novels/${novel.id}`)
+      .json(payload)
+      .loginAs(user)
+
+    response.assertStatus(403)
+    response.assertAgainstApiSpec()
+  })
+
+  test("guest cannot update their novel", async ({ client }) => {
+    const user = await UserFactory
+      .with("emailVerificationTokens", 1, (token) => token.apply("verified"))
+      .with("novels", 1)
+      .create()
+
+    const novel = await user.related("novels").query().firstOrFail()
+
+    const payload = {
+      title: "someRandomTitle",
+      content: [
+        {
+          title: "A new day",
+          content: "Once there was an innocent man..."
+        },
+        {
+          title: "At dawn's end",
+          content: "... but alas, everything comes to an end."
+        }
+      ],
+      tags: ["self-discovery", "adventure"]
+    }
+
+    const response = await client
+      .put(`/api/v1/user/novels/${novel.id}`)
+      .json(payload)
+
+    response.assertStatus(401)
+    response.assertAgainstApiSpec()
+  })
+
 })
