be1fe16324acd1f8487c96a49f920f4bdd4082f1
Hilary <hilarykodji@gmail.com>
2022-09-01 00:43:42 +0000

    [fix] - fix build error. Remove cat of .env file

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UsersController {
-    public async show({ auth, bouncer, request, response }: HttpContextContract) {
+    public async show({ auth, response }: HttpContextContract) {
         const user = auth.user!
         return response.ok(user.serialize())
     }
